ac566e69c8b4b219e8639b58ac3c4d89f0cdbbcc
Mario Olinto <marioolinto@gmail.com>
2018-03-15 17:56:32 -0400

    Finished the routing exercise

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,22 +11,15 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
   heroes: Hero[];
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void{
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
+  
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();  
   }
 
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
